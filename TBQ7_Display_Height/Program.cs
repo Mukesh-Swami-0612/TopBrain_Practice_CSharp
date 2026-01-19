71d2e6408d9cc5f7b0beb3b8b4de092f2d1864df
Mukesh Kumar <mukesh704swami@gmail.com>
2026-01-19 12:29:19 +0530

    Added TBQ7 practice questions

---
+++ TBQ7_Display_Height/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void checkHeight(int h)
+    {
+        if (h == -1)
+        {
+            Console.WriteLine("Thanks");
+            return;
+        }
+        else
+        {
+            if (h < 150)
+            {
+                Console.WriteLine("Short height");
+            }
+            else if (h >= 150 && h <= 180)
+            {
+                Console.WriteLine("Average Height");
+            }
+            else
+            {
+                Console.WriteLine("Tall height");
+            }
+        }
+
+        int hNew = int.Parse(Console.ReadLine());
+        checkHeight(hNew);
+    }
+
+    public static void Main(string[] args)
+    {
+        int h = int.Parse(Console.ReadLine());
+        checkHeight(h);
+    }
+}
