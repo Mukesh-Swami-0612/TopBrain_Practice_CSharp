f9c9fa2d338ff7e028151de86b7a18c30b4d5b0e
Mukesh Kumar <mukesh704swami@gmail.com>
2026-01-19 11:46:05 +0530

    Added TBQ6 practice questions

---
+++ TBQ6_LargestofThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static int compare(int a, int b, int c)
+    {
+        if (a > b)
+        {
+            if (a > c)
+                return a;
+            else
+                return c;
+        }
+        else
+        {
+            if (b > c)
+                return b;
+            else
+                return c;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int result = compare(a, b, c);
+        Console.WriteLine($"Largest of {a}, {b}, {c} is: {result}");
+    }
+}
