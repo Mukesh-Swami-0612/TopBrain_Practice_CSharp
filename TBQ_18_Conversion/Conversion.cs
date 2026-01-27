b96f393534ab2a8962bc53eb2c1433e5ba7c2e7a
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2026-01-27 19:34:29 +0530

    Create Conversion.cs

---
+++ TBQ_18_Conversion/Conversion.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Solution
+{
+    public static double ConvertFeetToCentimeters(int feet)
+    {
+        double centimeters = feet * 30.48;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+}
