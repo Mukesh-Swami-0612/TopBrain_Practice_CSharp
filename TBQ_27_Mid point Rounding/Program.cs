ba91670e376f75d009d1fba15d57cc43d00ac3d6
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2026-02-03 15:20:01 +0530

    Update Program.cs

--- TBQ_27_Mid point Rounding/Program.cs
+++ TBQ_27_Mid point Rounding/Program.cs
@@ -1,1 +1,23 @@
 
+using System;
+
+class Program
+{
+    public static double GetCircleArea(double radius)
+    {
+        // Calculate area
+        double area = Math.PI * radius * radius;
+
+        // Round to 2 decimals (AwayFromZero)
+        return Math.Round(area, 2, MidpointRounding.AwayFromZero);
+    }
+
+    static void Main()
+    {
+        double r = 5.5;
+
+        double result = GetCircleArea(r);
+
+        Console.WriteLine(result);
+    }
+}
